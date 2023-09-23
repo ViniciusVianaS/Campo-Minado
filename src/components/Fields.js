0e54676f48eaff6c87952dfe2d28b72931f253df
vinicius viana <viniciuspro333@gmail.com>
2023-09-22 23:46:02 -0300

    Campo Minado

---
+++ src/components/Fields.js
@@ -0,0 +1,65 @@
+import { View, StyleSheet, Text, TouchableWithoutFeedback } from "react-native";
+
+import params from "../params";
+import Mine from "./Mine";
+import Flag from "./Flag";
+
+
+export default props => {
+  const {mined, opened, nearMines, exploded, flagged} = props
+
+  const stylesField = [styles.field]
+  if (opened) stylesField.push(styles.opened)
+  if (exploded) stylesField.push(styles.exploded)
+  if (flagged) stylesField.push(styles.flagged)
+  if (!opened && !exploded) stylesField.push(styles.regular)
+
+  let color = null
+  if (nearMines > 0) {
+    if (nearMines == 1) color = '#2A28D7'
+    if (nearMines == 2) color = '#2B520F'
+    if (nearMines > 2 && nearMines < 6) color = '#F9060A'
+    if (nearMines >= 6) color = '#F221A9'
+  }
+
+  return (
+    <TouchableWithoutFeedback onPress={props.onOpen} onLongPress={props.onSelect}>
+      <View style={stylesField}>
+        {!mined && opened && nearMines> 0 ? 
+            <Text style={[styles.label, {color: color }]}>
+                {nearMines}</Text> : false}
+        {mined && opened ? <Mine/> : false}
+        {flagged && !opened ? <Flag /> : false}
+      </View>
+    </TouchableWithoutFeedback>
+  )
+}
+
+const styles = StyleSheet.create({
+  field: {
+    height: params.blockSize,
+    width: params.blockSize,
+    borderWidth: params.borderSize
+  },
+  regular: {
+    backgroundColor: '#999',
+    borderLeftColor: '#CCC',
+    borderTopColor: '#CCC',
+    borderRightColor: '#333',
+    borderBottomColor: '#333',
+  },
+  opened: {
+    backgroundColor: '#999',
+    borderColor: '#777',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  label: {
+    fontWeight: 'bold',
+    fontSize: params.fontSize,
+  },
+  exploded: {
+    backgroundColor: 'red',
+    borderColor: 'red',
+  }
+})
